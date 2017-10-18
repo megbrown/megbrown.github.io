ad594f432ec3ad6fdbe5f27e0d0c809c5b4033d9
Megan B <brown.m.j@outlook.com>
2017-10-18 09:20:07 -0500

    insert technologies with jQuery

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+	$.getJSON("../data/technologies.json", function(data) {
+		$.each(data, function (i, obj) {
+			console.log(obj);
+			let listItem = `<div class="col-sm-2 tech-logo-cont text-center">
+						<img class="img-fluid tech-logo" src="./images/logos/${obj.image}.png">
+						<h5 class="logo-title">${obj.name}</h5>`;
+			$('#techListContainer').append(listItem);
+		});
+	});
+});
